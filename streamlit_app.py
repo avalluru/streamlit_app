bfa7bb52195a0d20ae2a0a1b00a92f88e6bb82e0
avalluru <abhinavvalluru@gmail.com>
2023-11-11 11:10:36 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,43 @@
+import streamlit as st
+import pandas as pd
+import lightgbm as lgb
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import LabelEncoder
 
+# Assuming 'model' is a pre-trained machine learning model
+# You should load your trained model here
+# For demonstration, we'll initialize a LightGBM model
+# In practice, you would load a model with something like: model = joblib.load('model.pkl')
+model = lgb.LGBMClassifier()
+
+# Streamlit application
+def main():
+    st.title('COPD Stage Prediction with Lung Assist')
+
+    # Step 1: Upload a CSV file
+    uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
+
+    if uploaded_file is not None:
+        input_data = pd.read_csv(uploaded_file)
+        st.write('Data successfully uploaded!')
+        
+        # For demonstration purposes, we'll split the uploaded data
+        # In practice, you would only call predict on the uploaded data
+        # Here we are assuming that input_data contains both features and target
+        X = input_data.drop('target', axis=1)  # Replace 'target' with the actual target column name
+        y = input_data['target']  # Replace 'target' with the actual target column name
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        
+        # Step 2: Fit the model on the uploaded file
+        model.fit(X_train, y_train)
+        
+        # Step 3: Predict and display the output
+        if st.button('Predict COPD Stages'):
+            y_pred = model.predict(X_test)
+            accuracy = accuracy_score(y_test, y_pred)
+            report = classification_report(y_test, y_pred)
+
+            st.write(f"Lung Assist classifies the patient's COPD classification as: {y_pred}")
+            st.write(f"Accuracy: {accuracy}")
+            st.write("Classification Report:")
+            st.write(report)
